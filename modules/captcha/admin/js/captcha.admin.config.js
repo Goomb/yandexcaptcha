0821d324009b608a47b935096223f34de8d5ca2b
Stepan Kashuba <ihaterabbids@gmail.com>
2015-07-12 14:09:22 +0500

    Скрывать поле ввода кода API в админке при неактивном пункте

--- modules/captcha/admin/js/captcha.admin.config.js
+++ modules/captcha/admin/js/captcha.admin.config.js
@@ -11,6 +11,9 @@
 if ($('select[name=type]').val() != 'reCAPTCHA') {
     $('#recaptcha_public_key, #recaptcha_private_key').hide();
 }
+if ($('select[name=type]').val() != 'yandexcaptcha') {
+    $('#yandexcaptcha_key').hide();
+}
 $('select[name=type]').change(function() {
     if ($(this).val() == 'reCAPTCHA') {
         $('#recaptcha_public_key, #recaptcha_private_key').show();
